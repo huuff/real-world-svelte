e59b1cb56e2cc465896168d8083bec1a9df86fbf
Haf <haf@protonmail.ch>
2024-02-03 23:37:34 +0100

    started with flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Real World Svelte book examples";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github.com/numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: 
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            nodejs_20
+            nodePackages_latest.pnpm
+          ];
+        };
+      });
+}
