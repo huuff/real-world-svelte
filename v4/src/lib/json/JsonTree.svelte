77a06464ca91f00cbf3bb2d63452c431736f7fad
Haf <haf@protonmail.ch>
2024-02-05 22:14:34 +0100

    doin json tree

---
+++ v4/src/lib/json/JsonTree.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+ export let data: any;
+</script>
+
+<ul>
+    {#each Object.entries(data) as [key,value]}
+        <li>
+            {key}:
+            {#if typeof value == "object" }
+                <svelte:self data={value} />
+            {:else}
+                {value}
+            {/if}
+        </li>
+    {/each}
+</ul>
---
+++ v4/src/routes/ch4/json/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+ import JsonTree from "$lib/json/JsonTree.svelte";
+ 
+ const data = {
+     name: "John Doe",
+     address: {
+         street: "123 Main St",
+     }
+ }
+</script>
+
+<JsonTree data={data} />
