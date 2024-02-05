afd15d2aa8b391d98c4b897a5d777f181241a63e
Haf <haf@protonmail.ch>
2024-02-05 07:10:34 +0100

    actually showing popups

--- v4/src/lib/popup.ts
+++ v4/src/lib/popup.ts
@@ -1,4 +1,4 @@
-import { get, writable, derived } from "svelte/store";
+import { writable, derived } from "svelte/store";
 
 export function createPopupManager() {
   const count = writable(0);
@@ -7,7 +7,5 @@ export function createPopupManager() {
   const createPopup = () => count.update(it => it+1);
   const removePopup = () => count.update(it => it > 0 ? it-1 : it );
 
-  // const isLocked = () => get(count) > 0;
-
-  return { createPopup, removePopup, isLocked }
+  return { createPopup, count, removePopup, isLocked }
 }
