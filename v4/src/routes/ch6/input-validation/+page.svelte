bb5e1fdd96a4a6c6e9ee71d9d4a9d28a3d296fa5
Haf <haf@protonmail.ch>
2024-02-07 05:56:51 +0100

    fucked up formatting

--- v4/src/routes/ch6/input-validation/+page.svelte
+++ v4/src/routes/ch6/input-validation/+page.svelte
@@ -1,33 +1,33 @@
 <script lang="ts">
-    import type { Action } from 'svelte/action';
+	import type { Action } from 'svelte/action';
 
-    const validateOnType: Action<HTMLInputElement> = (node) => {
-            const onInput = () => {
-                    // TODO: Try to use 'this'?
-                    const isValid = node.checkValidity();
-                    const errorMessage = isValid ? '' : 'ERROR!!!!!!!!';
+	const validateOnType: Action<HTMLInputElement> = (node) => {
+		const onInput = () => {
+			// TODO: Try to use 'this'?
+			const isValid = node.checkValidity();
+			const errorMessage = isValid ? '' : 'ERROR!!!!!!!!';
 
-                    node.dispatchEvent(new CustomEvent('validate', { detail: { isValid, errorMessage } }));
-            };
-            node.addEventListener('input', onInput);
+			node.dispatchEvent(new CustomEvent('validate', { detail: { isValid, errorMessage } }));
+		};
+		node.addEventListener('input', onInput);
 
-            return {
-                    destroy: () => node.removeEventListener('input', onInput)
-            };
-    };
+		return {
+			destroy: () => node.removeEventListener('input', onInput)
+		};
+	};
 
- let isValid = false;
+	let isValid = false;
 </script>
 
-<style>
- .invalid {
-     border: red 1px solid;
- }
-</style>
-
 <input
-    required
-    class:invalid={!isValid}
-    use:validateOnType
-    on:validate={(event) => isValid = event.detail.isValid}
-  />
+	required
+	class:invalid={!isValid}
+	use:validateOnType
+	on:validate={(event) => (isValid = event.detail.isValid)}
+/>
+
+<style>
+	.invalid {
+		border: red 1px solid;
+	}
+</style>
