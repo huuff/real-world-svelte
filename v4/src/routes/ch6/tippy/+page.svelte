7cb91a06f12a0eb6c5490d4cf2fee0f8eeeb758a
Haf <haf@protonmail.ch>
2024-02-08 05:58:16 +0100

    tippyjs

---
+++ v4/src/routes/ch6/tippy/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Action } from "svelte/action";
+  import tippy from "tippy.js";
+  import "tippy.js/dist/tippy.css";
+
+  const tippyAction: Action<HTMLElement, string> = (element, content) => {
+    const tooltipInstance = tippy(element, { content });
+    return {
+      update: (newContent) => {
+        tooltipInstance.setContent(newContent);
+      },
+      destroy: () => {
+        tooltipInstance.destroy();
+      },
+    };
+  };
+</script>
+
+<div use:tippyAction={"and this is the tooltip"}>This has a tooltip</div>
