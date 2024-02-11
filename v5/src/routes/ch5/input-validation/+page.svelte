2a6d55717b331847d18e5317a7d162601b5c4557
Haf <haf@protonmail.ch>
2024-02-11 11:46:01 +0100

    svelte5 validated input

---
+++ v5/src/routes/ch5/input-validation/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import ValidatedInput from "$lib/ValidatedInput.svelte";
+
+  let classname = $state("");
+
+  const validityChangeHandler = (newValidity: boolean) => {
+    classname = newValidity ? "" : "invalid";
+  };
+</script>
+
+<ValidatedInput class={classname} onvaliditychange={validityChangeHandler} required />
+
+<style>
+  :global(.invalid) {
+    outline: 1px solid red;
+  }
+</style>
